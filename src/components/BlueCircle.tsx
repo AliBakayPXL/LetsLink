3250face71264a9286b86d9995beff33fd1d0994
Ali Bakay <ali.bakay@outlook.be>
2025-05-07 11:18:43 +0200

    Branch aangemaakt: Homepage

--- src/components/BlueCircle.tsx
+++ src/components/BlueCircle.tsx
@@ -8,7 +8,7 @@ interface BlueCircleProps {
 const BlueCircle = ({ children, className = "" }: BlueCircleProps) => (
   <span
     className={
-      "bg-[#053241] rounded-full p-3 mr-4 inline-flex items-center justify-center " +
+      "bg-[#053242] rounded-full p-3 mr-4 inline-flex items-center justify-center " +
       className
     }
   >
