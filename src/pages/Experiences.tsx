ef091b25edea76519ec01bb23c8009ea20a8556f
JorgoVdP <Jorgo.VandeParre@student.pxl.be>
2025-05-07 12:57:26 +0200

    added 2 components for experience V1

--- src/pages/Experiences.tsx
+++ src/pages/Experiences.tsx
@@ -1,10 +1,13 @@
-import React from "react";
 import ExperienceHero from "../components/ExperienceHero";
+import ExperienceNetworkEvents from "../components/ExperienceNetworkEvents.tsx";
+import ExperienceNetworkCorp from "../components/ExperienceNetworkCorp.tsx";
 
 const Experiences = () => {
   return (
   <main>
     <ExperienceHero/>
+    <ExperienceNetworkEvents/>
+    <ExperienceNetworkCorp/>
   </main>
   )
 };
