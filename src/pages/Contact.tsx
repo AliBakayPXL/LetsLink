0106e5fbf8a7033f2d22efaaa9730b3e3645a62e
JorgoVdP <Jorgo.VandeParre@student.pxl.be>
2025-05-08 11:23:18 +0200

    contactpage done

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import ContactForm from "../components/ContactForm";
+import ExperienceNavbar from "../components/ExperienceNavbar";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <main className="pt-24">
+      <ExperienceNavbar/>
+      <ContactForm></ContactForm>
+    </main>
+    )
 };
 
 export default Contact;
