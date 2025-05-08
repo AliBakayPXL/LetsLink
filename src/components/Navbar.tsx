290bbeddfbfd2930bdc0ccb21397978cd48df508
JorgoVdP <Jorgo.VandeParre@student.pxl.be>
2025-05-08 14:56:34 +0200

    navbar big button fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -196,7 +196,7 @@ const Navbar = () => {
 
           {/* Desktop Button */}
           <Link
-            to="/"
+            to="/Contact#contact"
             className="hidden lg:block bg-white/10 outline-1 outline-white/40 outline-offset-[-1px] px-7 py-2.5 rounded-full transition-all duration-200 hover:bg-[#3CACD2] hover:outline-none hover:text-primary whitespace-nowrap"
           >
             Book a Call
