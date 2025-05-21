1c3a18f40cc186f12400df299d8b31599a174373
JorgoVdP <Jorgo.VandeParre@student.pxl.be>
2025-05-21 16:53:14 +0200

    cookie page done

--- src/pages/Cookies.tsx
+++ src/pages/Cookies.tsx
@@ -1,11 +1,12 @@
 import ExperienceNavbar from "../components/ExperienceNavbar";
+import CookieContext from "../components/CookieContext";
 const Cookies = () => {
   
 
     return (
     <main className="pt-24" id="hero">
         <ExperienceNavbar></ExperienceNavbar>
-        <p>test</p>
+        <CookieContext></CookieContext>
     </main>
     )
   };
